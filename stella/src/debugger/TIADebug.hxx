0bc1dc961a93a73645c015b3201ae217640d1e31
urchlay <urchlay@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2005-07-18 16:10:52 +0000

    Added --enable-static option to ./configure script. Note that you must have static versions of all the libraries (like libSDL.a, libz.a, libpng.a), or else you'll end up with the dynamic version(s) of them... so you'd have a stella binary that's almost-static.
    Also, the only way I could get things to link properly was to include
    -lz in the LIBS twice: once before and once after -lpng. Removing either
    one causes the link to fail on my Linux box. This could cause issues
    for other systems though, am not sure...
    
    
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@671 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- stella/src/debugger/TIADebug.hxx
+++ stella/src/debugger/TIADebug.hxx
@@ -13,7 +13,7 @@
 // See the file "license" for information on usage and redistribution of
 // this file, and for a DISCLAIMER OF ALL WARRANTIES.
 //
-// $Id: TIADebug.hxx,v 1.9 2005-07-15 18:19:29 stephena Exp $
+// $Id: TIADebug.hxx,v 1.10 2005-07-18 16:10:52 urchlay Exp $
 //============================================================================
 
 #ifndef TIA_DEBUG_HXX
@@ -24,6 +24,15 @@ class Debugger;
 
 #include "Array.hxx"
 #include "DebuggerSystem.hxx"
+
+/*
+// pointer types for TIADebug instance methods
+// (used by IntMethodExpression)
+typedef int (TiaDebug::*TIADEBUG_INT_METHOD)();
+
+// call the pointed-to method on the (global) CPU debugger object.
+#define CALL_TIADEBUG_METHOD(method) ( ( Debugger::debugger().tiaDebug().*method)() )
+*/
 
 class TiaState : public DebuggerState
 {
