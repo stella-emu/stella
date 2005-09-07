6b0225507d6f95f937ff5f6faed82ecdb7dd0013
markgrebe <markgrebe@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2005-09-07 03:11:33 +0000

    Add header file for Desktop Display Size Functions
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@770 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

---
+++ stella/src/macosx/macOSXDisplay.h
@@ -0,0 +1,14 @@
+/* macOSXDisplay.h - Header for display size 
+   support functions for the
+   Macintosh OS X SDL port of Stella
+   Mark Grebe <atarimac@cox.net>
+   
+*/
+/* $Id: macOSXDisplay.h,v 1.1 2005-09-07 03:11:33 markgrebe Exp $ */
+
+
+extern "C" {
+uInt16 macOSXDisplayWidth(void);
+uInt16 macOSXDisplayHeight(void);
+}
+
