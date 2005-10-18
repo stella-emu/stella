064eda4e580e52711710e7dd8304d677eebb7a70
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2005-10-18 19:04:56 +0000

    Some obvious fixes.
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@850 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- stella/src/win32/SettingsWin32.cxx
+++ stella/src/win32/SettingsWin32.cxx
@@ -13,7 +13,7 @@
 // See the file "license" for information on usage and redistribution of
 // this file, and for a DISCLAIMER OF ALL WARRANTIES.
 //
-// $Id: SettingsWin32.cxx,v 1.19 2005-10-18 18:49:46 stephena Exp $
+// $Id: SettingsWin32.cxx,v 1.20 2005-10-18 19:04:56 stephena Exp $
 //============================================================================
 
 #include <sstream>
@@ -28,9 +28,9 @@
 SettingsWin32::SettingsWin32(OSystem* osystem)
     : Settings(osystem)
 {
-  set("fragsize", "2048");    // Anything less than this usually causes sound skipping
-  set("video", "hard");       // Use software mode with hardware surface
-  set("dirtyrects", "false")  // Most Windows systems work better without this
+  set("fragsize", "2048");     // Anything less than this usually causes sound skipping
+  set("video", "hard");        // Use software mode with hardware surface
+  set("dirtyrects", "false");  // Most Windows systems work better without this
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
