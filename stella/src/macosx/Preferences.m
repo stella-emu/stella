d06a92762b68210d490f07066426b004277ec11b
markgrebe <markgrebe@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2006-01-15 02:12:09 +0000

    Fixed issue with encoding that appeared with OSX 10.4.4
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@962 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- stella/src/macosx/Preferences.m
+++ stella/src/macosx/Preferences.m
@@ -7,7 +7,7 @@
    TextEdit application.
 
 */
-/* $Id: Preferences.m,v 1.1.1.1 2004-06-16 02:30:30 markgrebe Exp $ */
+/* $Id: Preferences.m,v 1.2 2006-01-15 02:12:09 markgrebe Exp $ */
 
 #import <Cocoa/Cocoa.h>
 #import "Preferences.h"
@@ -65,7 +65,7 @@ static Preferences *sharedInstance = nil;
 	if (theValue == nil)
 		value[0] = 0;
 	else {
-		[theValue getCString:value maxLength:1023];
+		[theValue getCString:value maxLength:1023 encoding:NSASCIIStringEncoding];
 		[theKey release];
 		}
 }
