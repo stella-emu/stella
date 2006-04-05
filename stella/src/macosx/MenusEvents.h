b6b2e99724010438fd08a52e4ca8284fb600299e
markgrebe <markgrebe@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2006-04-05 02:31:26 +0000

    Changes for Menu Enabling/Disabling and sending events instead of keypress from the Menu code.
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@1076 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

---
+++ stella/src/macosx/MenusEvents.h
@@ -0,0 +1,14 @@
+/* MenusEvents.h - Menus window 
+   class defines for event sending between Objective-C
+   and C++
+   Macintosh OS X SDL port of Stella
+   Mark Grebe <atarimac@cox.net>
+   
+*/
+/* $Id: MenusEvents.h,v 1.12 2006/03/29 04:45:10 markgrebe Exp $ */
+ 
+#define MENU_PAUSE              1
+#define MENU_OPEN               2
+#define MENU_VOLUME_INCREASE    3
+#define MENU_VOLUME_DECREASE    4
+
