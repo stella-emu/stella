d4792432072d1b55023c6b535da83a741d145e0e
azaballa <azaballa@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2006-04-12 22:50:18 +0000

    Synced the tia and samplerate frequencies to 15700 to correct a sound error in Ms. Pacman.
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@1086 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- stella/src/gp2x/SettingsGP2X.cxx
+++ stella/src/gp2x/SettingsGP2X.cxx
@@ -13,7 +13,7 @@
 // See the file "license" for information on usage and redistribution of
 // this file, and for a DISCLAIMER OF ALL WARRANTIES.
 //
-// $Id: SettingsGP2X.cxx,v 1.8 2006-03-15 21:39:48 azaballa Exp $
+// $Id: SettingsGP2X.cxx,v 1.9 2006-04-12 22:50:18 azaballa Exp $
 // Modified on 2006/02/05 by Alex Zaballa for use on GP2X
 //============================================================================
 
@@ -32,8 +32,8 @@ SettingsGP2X::SettingsGP2X(OSystem* osystem)
   setInternal("sound", "true");
   setInternal("zoom", "1");
   setInternal("fragsize", "512");
-  setInternal("tiafreq", "22050");
-  setInternal("clipvol", "false");
+  setInternal("tiafreq", "11025");
+  setInternal("clipvol", "true");
   setInternal("joymouse", "true");
   setInternal("rombrowse", "true");
 }
