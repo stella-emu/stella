1344ad67c9f36d4ff3ceb67600e101242d2bcd5a
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2006-12-23 15:08:46 +0000

    Cleaned up the debugger files a great deal.  There was no need to have each different Expression class in a separate file, especially since they're mostly only one-line methods.  So now all the classes are placed in DebuggerExpressions.hxx.
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@1234 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- stella/src/debugger/module.mk
+++ stella/src/debugger/module.mk
@@ -5,37 +5,6 @@ MODULE_OBJS := \
 	src/debugger/DebuggerParser.o \
 	src/debugger/EquateList.o \
 	src/debugger/Expression.o \
-	src/debugger/FunctionExpression.o \
-	src/debugger/CpuMethodExpression.o \
-	src/debugger/TiaMethodExpression.o \
-	src/debugger/ByteDerefExpression.o \
-	src/debugger/ByteDerefOffsetExpression.o \
-	src/debugger/WordDerefExpression.o \
-	src/debugger/ConstExpression.o \
-	src/debugger/EquateExpression.o \
-	src/debugger/LoByteExpression.o \
-	src/debugger/HiByteExpression.o \
-	src/debugger/NotEqualsExpression.o \
-	src/debugger/EqualsExpression.o \
-	src/debugger/PlusExpression.o \
-	src/debugger/UnaryMinusExpression.o \
-	src/debugger/MinusExpression.o \
-	src/debugger/MultExpression.o \
-	src/debugger/DivExpression.o \
-	src/debugger/ModExpression.o \
-	src/debugger/LogAndExpression.o \
-	src/debugger/LogOrExpression.o \
-	src/debugger/LogNotExpression.o \
-	src/debugger/BinNotExpression.o \
-	src/debugger/BinAndExpression.o \
-	src/debugger/BinOrExpression.o \
-	src/debugger/BinXorExpression.o \
-	src/debugger/GreaterExpression.o \
-	src/debugger/GreaterEqualsExpression.o \
-	src/debugger/LessExpression.o \
-	src/debugger/LessEqualsExpression.o \
-	src/debugger/ShiftRightExpression.o \
-	src/debugger/ShiftLeftExpression.o \
 	src/debugger/PackedBitArray.o \
 	src/debugger/CpuDebug.o \
 	src/debugger/RamDebug.o \
