22311086fde72d3a5935a4c7a4183e201f9eac1b
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2009-04-15 12:43:22 +0000

    Bumped version # for next release.
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@1707 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- stella/src/common/Version.hxx
+++ stella/src/common/Version.hxx
@@ -13,13 +13,13 @@
 // See the file "license" for information on usage and redistribution of
 // this file, and for a DISCLAIMER OF ALL WARRANTIES.
 //
-// $Id: Version.hxx,v 1.56 2009-04-13 16:21:42 stephena Exp $
+// $Id: Version.hxx,v 1.57 2009-04-15 12:43:22 stephena Exp $
 //============================================================================
 
 #ifndef VERSION_HXX
 #define VERSION_HXX
 
-#define STELLA_BASE_VERSION "2.7.6"
+#define STELLA_BASE_VERSION "2.7.7_cvs"
 
 #ifdef NIGHTLY_BUILD
   #define STELLA_VERSION STELLA_BASE_VERSION "pre-" NIGHTLY_BUILD
