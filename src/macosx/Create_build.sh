fd61491f1ec3878c56cd20f397c7ca8b5ac05ec2
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2009-09-11 15:37:08 +0000

    Updated OSX project files for 3.0 release, formatted TXT files for better viewing in OSX default text editor.
    Added build script and template DMG to automatically build a
    StellaOSXver.dmg file.
    
    
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@1880 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

---
+++ src/macosx/Create_build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Creates a StellaOSX disk image (dmg) from the command line.
+# usage:
+#    Create_build.sh <version>
+#
+# The result will be a file called ~/Desktop/StellaOSX<ver>.dmg
+
+if [ $# != 1 ]; then
+	echo "usage: Create_build.sh version"
+	exit 0
+fi
+
+VER="$1"
+DMG="StellaOSX${VER}.dmg"
+DISK="/Volumes/StellaOSX"
+
+echo "Creating ${DMG} file ..."
+gunzip -c template.dmg.gz > ${DMG}
+
+echo "Mounting ${DMG} file ..."
+hdiutil attach ${DMG}
+
+echo "Copying documentation ..."
+ditto ../../Announce.txt ../../Changes.txt ../../Copyright.txt ../../License.txt ../../Readme.txt ../../Todo.txt ${DISK}
+
+echo "Copying application ..."
+cp -r build/Deployment/StellaOSX.app ${DISK}
+
+echo "Ejecting ${DMG} ..."
+hdiutil eject ${DISK}
+
+echo "Compressing image, moving to Desktop ..."
+hdiutil convert ${DMG} -format UDZO -imagekey zlib-level=9 -o ~/Desktop/${DMG}
+rm -f ${DMG}
