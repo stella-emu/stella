d9c05df0f2d6d13d07377316e4a4ae9c68237830
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2013-03-14 12:25:35 +0000

    Uninitialized variable caused disassembly failure on 4K ROMs.
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@2673 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- src/debugger/CartDebug.cxx
+++ src/debugger/CartDebug.cxx
@@ -50,15 +50,14 @@ CartDebug::CartDebug(Debugger& dbg, Console& console, const OSystem& osystem)
   // Banksizes greater than 4096 indicate multi-bank ROMs, but we handle only
   // 4K pieces at a time
   // Banksizes less than 4K use the actual value
-  int banksize = 4096;
-  myConsole.cartridge().getImage(banksize);
-  banksize = BSPF_min(banksize, 4096);
   BankInfo info;
+  info.start = info.end = info.offset = 0;
+  int banksize = 0;
+  myConsole.cartridge().getImage(banksize);
+  info.size = BSPF_min(banksize, 4096);
+
   for(int i = 0; i < myConsole.cartridge().bankCount(); ++i)
-  {
-    info.size = banksize;
     myBankInfo.push_back(info);
-  }
   info.size = 128;  // ZP RAM
   myBankInfo.push_back(info);
 
