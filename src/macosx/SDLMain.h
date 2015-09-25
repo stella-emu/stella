573570c1c8a8173f1e5e71f0197b74887a5bdf8b
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2015-09-25 22:45:44 +0000

    Fairly large overhaul of the OSX SDLmain.m file, removing redundant code that was used for SDL 1.2, etc.  It is now much easier to understand and modify (but I *still* hate Objective-C).
    git-svn-id: svn://svn.code.sf.net/p/stella/code/trunk@3217 8b62c5a3-ac7e-4cc8-8f21-d9a121418aba

--- src/macosx/SDLMain.h
+++ src/macosx/SDLMain.h
@@ -1,22 +1,14 @@
-/*   SDLMain.h - main entry point for our Cocoa-ized SDL app
-       Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
-       Non-NIB-Code & other changes: Max Horn <max@quendi.de>
-
-    Feel free to customize this file to suit your needs
-*/
-/* $Id$ */
+/** SDLMain.m - main entry point for our Cocoa-ized SDL app
+    Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
+    Non-NIB-Code & other changes: Max Horn <max@quendi.de>
+  */
+// $Id$
 
 #import <Cocoa/Cocoa.h>
 
 @interface SDLMain : NSObject
 {
 }
-+ (SDLMain *)sharedInstance;
-
-@end
-
-@interface SDLApplication : NSApplication {
-
-}
++ (SDLMain*) sharedInstance;
 
 @end
