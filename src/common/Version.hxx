6f16ee9c0b311fc923c803b1d3d486ac831a4d86
Stephen Anthony <sa666666@gmail.com>
2017-05-29 16:30:42 -0230

    Bumped version # for another pre-release.

--- src/common/Version.hxx
+++ src/common/Version.hxx
@@ -18,7 +18,7 @@
 #ifndef VERSION_HXX
 #define VERSION_HXX
 
-#define STELLA_VERSION "5.0.0-pre8"
-#define STELLA_BUILD "3399"
+#define STELLA_VERSION "5.0.0-pre9"
+#define STELLA_BUILD "3402"
 
 #endif
