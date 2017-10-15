5317c86f23a502a18d2e0e0601dbeca53b929c9c
Stephen Anthony <sa666666@gmail.com>
2017-10-15 17:58:34 -0230

    State files are no longer valid after the frame-manager refactoring.

--- src/common/StateManager.cxx
+++ src/common/StateManager.cxx
@@ -28,7 +28,7 @@
 
 #include "StateManager.hxx"
 
-#define STATE_HEADER "05000302state"
+#define STATE_HEADER "05000303state"
 // #define MOVIE_HEADER "03030000movie"
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
