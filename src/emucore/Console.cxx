1c10f55bb093b52a983750cd5deecce7bcb1169e
Christian Speckner <christian.speckner@mayflower.de>
2017-11-26 23:10:43 +0100

    Mute audio while autodetection runs.

--- src/emucore/Console.cxx
+++ src/emucore/Console.cxx
@@ -117,6 +117,9 @@ Console::Console(OSystem& osystem, unique_ptr<Cartridge>& cart,
   const string& md5 = myProperties.get(Cartridge_MD5);
   setControllers(md5);
 
+  // Mute audio while autodetection runs
+  myOSystem.sound().mute(1);
+
   if(myDisplayFormat == "AUTO" || myOSystem.settings().getBool("rominfo"))
   {
     autodetectFrameLayout();
