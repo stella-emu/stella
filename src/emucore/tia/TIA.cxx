ba549e1d8ae957133582434437e99df2ea1ff693
Christian Speckner <christian.speckner@mayflower.de>
2018-02-04 23:18:30 +0100

    Improve PF collision handling during hblank.

--- src/emucore/tia/TIA.cxx
+++ src/emucore/tia/TIA.cxx
@@ -1229,6 +1229,8 @@ void TIA::tickHblank()
       if (myExtendedHblank) myHstate = HState::frame;
       break;
   }
+
+  if (myExtendedHblank && myHctr > 67) myPlayfield.tick(myHctr - 68 - myHctrDelta);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -1269,6 +1271,7 @@ void TIA::nextLine()
     cloneLastLine();
   }
 
+  myPlayfield.tick(0);
   myHctr = 0;
 
   if (!myMovementInProgress && myLinesSinceChange < 2) myLinesSinceChange++;
