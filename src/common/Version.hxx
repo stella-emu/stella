75a77b78e478ea08f9b175f40e9760b6160ea4dc
Stephen Anthony <sa666666@gmail.com>
2019-05-10 09:24:55 -0230

    Update for next beta release.

--- src/common/Version.hxx
+++ src/common/Version.hxx
@@ -18,7 +18,7 @@
 #ifndef VERSION_HXX
 #define VERSION_HXX
 
-#define STELLA_VERSION "6.1_beta1"
-#define STELLA_BUILD "4877"
+#define STELLA_VERSION "6.1_beta2"
+#define STELLA_BUILD "5067"
 
 #endif
