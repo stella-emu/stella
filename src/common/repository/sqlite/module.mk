c75fcfae13455227e520ee19ff3df20cca580d45
Christian Speckner <christian.speckner@mayflower.de>
2019-05-15 10:32:01 +0200

    RAII wrapper for sqlite transactions, better transaction handling in key value repo.

--- src/common/repository/sqlite/module.mk
+++ src/common/repository/sqlite/module.mk
@@ -4,7 +4,8 @@ MODULE_OBJS := \
 	src/common/repository/sqlite/KeyValueRepositorySqlite.o \
 	src/common/repository/sqlite/SettingsDb.o \
 	src/common/repository/sqlite/SqliteDatabase.o \
-	src/common/repository/sqlite/SqliteStatement.o
+	src/common/repository/sqlite/SqliteStatement.o \
+	src/common/repository/sqlite/SqliteTransaction.o
 
 MODULE_DIRS += \
 	src/common/repository/sqlite
