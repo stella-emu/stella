9b63a1e3d4bd8b8694d3b86b933d3b94cf468903
thrust26 <tjentzsch@yahoo.de>
2019-06-30 11:43:36 +0200

    fix several bugs

--- src/emucore/Event.hxx
+++ src/emucore/Event.hxx
@@ -125,7 +125,7 @@ class Event
     };
 
     // Event list version, update if the id of existing event types changed
-    static constexpr Int32 VERSION = 2;
+    static constexpr Int32 VERSION = 3;
 
     using EventSet = std::set<Event::Type>;
 
