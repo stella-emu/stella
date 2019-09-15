4b0f255b8df5b67178a2a84489a98842c70fa373
thrust26 <tjentzsch@yahoo.de>
2019-09-15 11:52:43 +0200

    more types for partial fix for issue #537 (RAM writes to read port)

--- src/emucore/CartFA.cxx
+++ src/emucore/CartFA.cxx
@@ -128,8 +128,19 @@ bool CartridgeFA::poke(uInt16 address, uInt8 value)
       break;
   }
 
-  pokeRAM(myRAM[address & 0x00FF], address, value);
-  return true;
+  if (!(address & 0x100))
+  {
+    pokeRAM(myRAM[address & 0x00FF], address, value);
+    return true;
+  }
+  else
+  {
+    // Writing to the read port should be ignored, but (TODO) trigger a break if option enabled
+    uInt8 dummy;
+
+    pokeRAM(dummy, address, value);
+    return false;
+  }
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
