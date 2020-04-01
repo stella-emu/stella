7b16436a84d3f869ca860a16abe0faeab3662ffb
Stephen Anthony <sa666666@gmail.com>
2020-04-01 09:45:19 -0230

    Fix warnings from clang.

--- src/emucore/Device.hxx
+++ src/emucore/Device.hxx
@@ -165,7 +165,7 @@ class Device : public Serializable
 
       @return  The access counters as comma separated string
     */
-    virtual string getAccessCounters() const { return ""; };
+    virtual string getAccessCounters() const { return ""; }
   #endif
 
   protected:
