e01d8e57a5ffaac672fc294141e270d725f33439
thrust26 <tjentzsch@yahoo.de>
2020-11-15 08:59:18 +0100

    fixed garbage in fullscreen mode fixed breakpoints flickering in RomListWidget fixed palette update in VideoAudioDialog

--- src/debugger/gui/RomListWidget.cxx
+++ src/debugger/gui/RomListWidget.cxx
@@ -491,6 +491,8 @@ void RomListWidget::drawWidget(bool hilite)
                              checkBreakPoint(dlist[pos].address,
                              instance().debugger().cartDebug().getBank(dlist[pos].address)));
     myCheckList[i]->setDirty();
+    // draw immediately, because chain order is not deterministic
+    myCheckList[i]->draw();
 
     // Draw highlighted item in a frame
     if(_highlightedItem == pos)
