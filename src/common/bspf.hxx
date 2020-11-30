2e0c0549e7d02079be096b6525a978640f6b33e1
Stephen Anthony <sa666666@gmail.com>
2020-11-30 16:08:56 -0330

    Move function to bspf.hxx.  It's not used yet, but it might be in the future.

--- src/common/bspf.hxx
+++ src/common/bspf.hxx
@@ -290,6 +290,19 @@ namespace BSPF
     return false;
   }
 
+  // Modify 'str', replacing all occurrences of 'from' with 'to'
+  inline void replaceAll(string& str, const string& from, const string& to)
+  {
+    if(from.empty()) return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != string::npos)
+    {
+      str.replace(start_pos, from.length(), to);
+      start_pos += to.length(); // In case 'to' contains 'from',
+                                // like replacing 'x' with 'yx'
+    }
+  }
+
   // C++11 way to get local time
   // Equivalent to the C-style localtime() function, but is thread-safe
   inline std::tm localTime()
