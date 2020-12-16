61d4f1d47fde64493a44e55fd0016ddce629b0a4
Stephen Anthony <sa666666@gmail.com>
2020-12-15 22:51:31 -0330

    Fix compile warnings in Linux.

--- src/sqlite/sqlite3.c
+++ src/sqlite/sqlite3.c
@@ -1,6 +1,37 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weverything"
+//============================================================================
+//
+//   SSSS    tt          lll  lll
+//  SS  SS   tt           ll   ll
+//  SS     tttttt  eeee   ll   ll   aaaa
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2020 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//============================================================================
 
-#include "source/sqlite3.c"
+#ifndef SQLITE_LIB_CXX
+#define SQLITE_LIB_CXX
 
-#pragma GCC diagnostics pop
+// Linux and libretro doesn't build unless this is enabled
+#define SQLITE_OMIT_LOAD_EXTENSION 1
+
+/*
+ * We can't control the quality of code from outside projects, so for now
+ * just disable warnings for it.
+ */
+#ifdef __clang__
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Weverything"
+  #include "source/sqlite3.c"
+  #pragma clang diagnostic pop
+#else
+  #include "source/sqlite3.c"
+#endif
+
+#endif  // SQLITE_LIB_CXX
--- src/sqlite/sqlite3.h
+++ src/sqlite/sqlite3.h
@@ -1,6 +1,34 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weverything"
+//============================================================================
+//
+//   SSSS    tt          lll  lll
+//  SS  SS   tt           ll   ll
+//  SS     tttttt  eeee   ll   ll   aaaa
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2020 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//============================================================================
 
-#include "source/sqlite3.h"
+#ifndef SQLITE_LIB_HXX
+#define SQLITE_LIB_HXX
 
-#pragma GCC diagnostics pop
+/*
+ * We can't control the quality of code from outside projects, so for now
+ * just disable warnings for it.
+ */
+#ifdef __clang__
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Weverything"
+  #include "source/sqlite3.h"
+  #pragma clang diagnostic pop
+#else
+  #include "source/sqlite3.h"
+#endif
+
+#endif  // SQLITE_LIB_HXX
