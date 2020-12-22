2c3d7e0baaf77d17c567990775467e24e005dc4b
thrust26 <tjentzsch@yahoo.de>
2020-12-22 21:07:19 +0100

    Revert "2nd try to get notifications"
    This reverts commit c024ce6f55df4ce87a6e283d7d35956a6744eb76.

--- src/common/bspf.hxx
+++ src/common/bspf.hxx
@@ -118,7 +118,7 @@ static const string EmptyString("");
 namespace BSPF
 {
   static constexpr float PI_f = 3.141592653589793238462643383279502884F;
-  stati2 constexpr double PI_d = 3.141592653589793238462643383279502884;
+  static constexpr double PI_d = 3.141592653589793238462643383279502884;
 
   // CPU architecture type
   // This isn't complete yet, but takes care of all the major platforms
