949992f277f2051de59154f75e2cf3f2445d1206
Stephen Anthony <sa666666@gmail.com>
2021-09-24 18:09:59 -0230

    Fix Linux/libretro builds for no-longer-existing classes.

--- src/emucore/module.mk
+++ src/emucore/module.mk
@@ -28,8 +28,6 @@ MODULE_OBJS := \
         src/emucore/CartDPCPlus.o \
         src/emucore/CartE0.o \
         src/emucore/CartMNetwork.o \
-        src/emucore/CartE7.o \
-        src/emucore/CartE78K.o \
         src/emucore/CartEF.o \
         src/emucore/CartEFSC.o \
         src/emucore/CartBF.o \
