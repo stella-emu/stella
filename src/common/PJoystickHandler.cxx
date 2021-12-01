stella-emu/stella

commit a7189ce7ec6fbf881868087f677dd5f438bc5112

fix attempt for #692

src/common/PJoystickHandler.cxx
@@ -123,8 +123,8 @@ int PhysicalJoystickHandler::add(const PhysicalJoystickPtr& stick)
   {
     StickInfo info("", stick);
     myDatabase.emplace(stick->name, info);
-    setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kEmulationMode, true);
-    setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kMenuMode, true);
+    setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kEmulationMode);
+    setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kMenuMode);
   }
 
   return stick->ID;

commit 706fe916a3adf0e20bac9703ea0e20fa6d25c03e

joystick: Add menuMode mappings before emulationMode mappings
For some controllers, the initial default (i.e. when the controller
is first attached) mappings seem to not work in emulation mode, but
work just fine in menu mode. The emulation mode mappings are shown
in the UI, but do not work until the second time the emulator is
started.

From debugging, even in emulation mode the joyMap is returning
menu bindings when in-game. Fix this bug by adding the emulation
mode mappings before the menu mode mappings.

Fixes: a7189ce7ec6f ("fix attempt for #692")
Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

src/common/PJoystickHandler.cxx
@@ -173,8 +173,8 @@ void PhysicalJoystickHandler::addToDatabase(const PhysicalJoystickPtr& stick)
   {
     StickInfo info("", stick);
     myDatabase.emplace(stick->name, info);
-    setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kEmulationMode);
     setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kMenuMode);
+    setStickDefaultMapping(stick->ID, Event::NoType, EventMode::kEmulationMode);
   }
 
   ostringstream buf;