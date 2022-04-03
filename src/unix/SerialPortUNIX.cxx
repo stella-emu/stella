a24d3e67bcb4427c8eb171c4ca90b66eff0ef2a3
Stephen Anthony <sa666666@gmail.com>
2022-04-03 20:36:39 -0230

    Minor 'const' updates to POSIX classes.

--- src/unix/SerialPortUNIX.cxx
+++ src/unix/SerialPortUNIX.cxx
@@ -104,14 +104,14 @@ StringList SerialPortUNIX::portNames()
 
   // Check if port is valid; for now that means if it can be opened
   // Eventually we may extend this to do more intensive checks
-  auto isPortValid = [](const string& port) {
+  const auto isPortValid = [](const string& port) {
     int handle = open(port.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
     if(handle > 0)  close(handle);
     return handle > 0;
   };
 
   // Get all possible devices in the '/dev' directory
-  FilesystemNode::NameFilter filter = [](const FilesystemNode& node) {
+  const FilesystemNode::NameFilter filter = [](const FilesystemNode& node) {
     return BSPF::startsWithIgnoreCase(node.getPath(), "/dev/ttyACM") ||
            BSPF::startsWithIgnoreCase(node.getPath(), "/dev/ttyUSB");
   };
