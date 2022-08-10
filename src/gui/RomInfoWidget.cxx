59fe1888c4d2426a73cbb047f76a42005735e179
Thomas Jentzsch <tjentzsch@yahoo.de>
2022-08-10 08:19:12 +0200

    preparing for #916

--- src/gui/RomInfoWidget.cxx
+++ src/gui/RomInfoWidget.cxx
@@ -24,21 +24,15 @@
 #include "ControllerDetector.hxx"
 #include "Bankswitch.hxx"
 #include "CartDetector.hxx"
-#include "Logger.hxx"
 #include "Props.hxx"
-#include "PNGLibrary.hxx"
 #include "PropsSet.hxx"
-#include "Rect.hxx"
-#include "Widget.hxx"
 #include "RomInfoWidget.hxx"
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 RomInfoWidget::RomInfoWidget(GuiObject* boss, const GUI::Font& font,
-                             int x, int y, int w, int h,
-                             const Common::Size& imgSize)
+                             int x, int y, int w, int h)
   : Widget(boss, font, x, y, w, h),
-    CommandSender(boss),
-    myAvail{imgSize}
+    CommandSender(boss)
 {
   _flags = Widget::FLAG_ENABLED;
   _bgcolor = kDlgColor;
@@ -64,9 +58,7 @@ void RomInfoWidget::setProperties(const FSNode& node, const string& md5)
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void RomInfoWidget::clearProperties()
 {
-  myHaveProperties = mySurfaceIsValid = false;
-  if(mySurface)
-    mySurface->setVisible(mySurfaceIsValid);
+  myHaveProperties = false;
 
   // Decide whether the information should be shown immediately
   if(instance().eventHandler().state() == EventHandlerState::LAUNCHER)
@@ -87,50 +79,8 @@ void RomInfoWidget::reloadProperties(const FSNode& node)
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void RomInfoWidget::parseProperties(const FSNode& node)
 {
-  // Check if a surface has ever been created; if so, we use it
-  // The surface will always be the maximum size, but sometimes we'll
-  // only draw certain parts of it
-  if(mySurface == nullptr)
-  {
-    mySurface = instance().frameBuffer().allocateSurface(
-        myAvail.w, myAvail.h, ScalingInterpolation::blur);
-    mySurface->applyAttributes();
-
-    dialog().addRenderCallback([this]() {
-      if(mySurfaceIsValid)
-        mySurface->render();
-      }
-    );
-  }
-
   // Initialize to empty properties entry
-  mySurfaceErrorMsg = "";
-  mySurfaceIsValid = false;
   myRomInfo.clear();
-
-#ifdef PNG_SUPPORT
-  // Get a valid filename representing a snapshot file for this rom and load the snapshot
-  const string& path = instance().snapshotLoadDir().getPath();
-
-  // 1. Try to load snapshot by property name
-  mySurfaceIsValid = loadPng(path + myProperties.get(PropType::Cart_Name) + ".png");
-
-  if(!mySurfaceIsValid)
-  {
-    // 2. If no snapshot with property name exists, try to load snapshot image by filename
-    mySurfaceIsValid = loadPng(path + node.getNameWithExt("") + ".png");
-
-    if(!mySurfaceIsValid)
-    {
-      // 3. If no ROM snapshot exists, try to load a default snapshot
-      mySurfaceIsValid = loadPng(path + "default_snapshot.png");
-    }
-  }
-#else
-  mySurfaceErrorMsg = "PNG image loading not supported";
-#endif
-  if(mySurface)
-    mySurface->setVisible(mySurfaceIsValid);
 
   myUrl = myProperties.get(PropType::Cart_Url);
 
@@ -207,36 +157,13 @@ void RomInfoWidget::parseProperties(const FSNode& node)
   setDirty();
 }
 
-#ifdef PNG_SUPPORT
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-bool RomInfoWidget::loadPng(const string& filename)
-{
-  try
-  {
-    instance().png().loadImage(filename, *mySurface);
-
-    // Scale surface to available image area
-    const Common::Rect& src = mySurface->srcRect();
-    const float scale = std::min(float(myAvail.w) / src.w(), float(myAvail.h) / src.h()) *
-      instance().frameBuffer().hidpiScaleFactor();
-    mySurface->setDstSize(static_cast<uInt32>(src.w() * scale), static_cast<uInt32>(src.h() * scale));
-
-    return true;
-  }
-  catch(const runtime_error& e)
-  {
-    mySurfaceErrorMsg = e.what();
-  }
-  return false;
-}
-#endif
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void RomInfoWidget::handleMouseUp(int x, int y, MouseButton b, int clickCount)
 {
-  if(isEnabled() && x >= 0 && x < _w && y >= 0 && y < _h)
+  if(isEnabled() && isHighlighted()
+    && x >= 0 && x < _w && y >= 0 && y < _h)
   {
-    clearFlags(Widget::FLAG_HILITED);
+    clearFlags(Widget::FLAG_HILITED); // avoid double clicks and opened URLs
     sendCommand(kClickedCmd, 0, _id);
   }
 }
@@ -245,11 +172,9 @@ void RomInfoWidget::handleMouseUp(int x, int y, MouseButton b, int clickCount)
 void RomInfoWidget::drawWidget(bool hilite)
 {
   FBSurface& s = dialog().surface();
-  const int yoff = myAvail.h + _font.getFontHeight() / 2;
 
   s.fillRect(_x+2, _y+2, _w-4, _h-4, _bgcolor);
-  s.frameRect(_x, _y, _w, myAvail.h, kColor);
-  s.frameRect(_x, _y+yoff, _w, _h-yoff, kColor);
+  s.frameRect(_x, _y, _w, _h, kColor);
 
   if(!myHaveProperties)
   {
@@ -257,27 +182,8 @@ void RomInfoWidget::drawWidget(bool hilite)
     return;
   }
 
-  if(mySurfaceIsValid)
-  {
-    const Common::Rect& dst = mySurface->dstRect();
-    const uInt32 scale = instance().frameBuffer().hidpiScaleFactor();
-    const uInt32 x = _x * scale + ((_w * scale - dst.w()) >> 1);
-    const uInt32 y = _y * scale + ((myAvail.h * scale - dst.h()) >> 1);
-
-    // Make sure when positioning the snapshot surface that we take
-    // the dialog surface position into account
-    const Common::Rect& s_dst = s.dstRect();
-    mySurface->setDstPos(x + s_dst.x(), y + s_dst.y());
-  }
-  else if(mySurfaceErrorMsg != "")
-  {
-    const uInt32 x = _x + ((_w - _font.getStringWidth(mySurfaceErrorMsg)) >> 1);
-    const uInt32 y = _y + ((yoff - _font.getLineHeight()) >> 1);
-    s.drawString(_font, mySurfaceErrorMsg, x, y, _w - 10, _textcolor);
-  }
-
   const int xpos = _x + 8;
-  int ypos = _y + yoff + 5;
+  int ypos = _y + 5;
   for(const auto& info : myRomInfo)
   {
     if(info.length() * _font.getMaxCharWidth() <= static_cast<size_t>(_w - 16))
