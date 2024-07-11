732d4ea4e12685d30e4a46d252b0d07ac1117fe1
Christian Speckner <christian.speckner@mayflower.de>
2024-07-11 22:40:00 +0200

    BL / B.W encoding tests 6 fixes.

--- src/emucore/elf/EncodingTest.cxx
+++ src/emucore/elf/EncodingTest.cxx
@@ -17,4 +17,42 @@
 
 #include <gtest/gtest.h>
 
-TEST(HelloWorld, Passes) {}
+#include "bspf.hxx"
+#include "ElfUtil.hxx"
+
+namespace {
+
+  struct Encoding {
+    Int32 offset;
+    uInt32 opcodeBL;
+    uInt32 opcodeB;
+  };
+
+  class EncodingTest: public testing::TestWithParam<Encoding> {};
+
+  TEST_P(EncodingTest, OffsetIsEncodedCorrectlyToBL) {
+    EXPECT_EQ(elfUtil::encode_B_BL(GetParam().offset - 4, true), GetParam().opcodeBL);
+  }
+
+  TEST_P(EncodingTest, OffsetIsEncodedCorrectlyToBW) {
+    EXPECT_EQ(elfUtil::encode_B_BL(GetParam().offset - 4, false), GetParam().opcodeB);
+  }
+
+  TEST_P(EncodingTest, OffsetIsDecodedCorrectlyFromBL) {
+    EXPECT_EQ(elfUtil::decode_B_BL(GetParam().opcodeBL), GetParam().offset - 4);
+  }
+
+  TEST_P(EncodingTest, OffsetIsDecodedCorrectlyFromB) {
+    EXPECT_EQ(elfUtil::decode_B_BL(GetParam().opcodeB), GetParam().offset - 4);
+  }
+
+  INSTANTIATE_TEST_SUITE_P(EncodingSuite, EncodingTest, testing::Values(
+    Encoding{.offset = 10, .opcodeBL = 0xf803f000, .opcodeB = 0xb803f000},
+    Encoding{.offset = 16777090, .opcodeBL = 0xd7bff3ff, .opcodeB = 0x97bff3ff},
+    Encoding{.offset = 8388606, .opcodeBL = 0xf7fdf3ff, .opcodeB = 0xb7fdf3ff},
+    Encoding{.offset = -10, .opcodeBL = 0xfff9f7ff, .opcodeB = 0xbff9f7ff},
+    Encoding{.offset = -16777090, .opcodeBL = 0xd03df400, .opcodeB = 0x903df400},
+    Encoding{.offset = -8388606, .opcodeBL = 0xdffff7ff, .opcodeB = 0x9ffff7ff}
+  ));
+
+}
