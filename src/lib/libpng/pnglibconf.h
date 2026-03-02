a224e7427d94a2464ec217dab7a8a7a41502b60a
Stephen Anthony <sa666666@gmail.com>
2026-03-02 14:09:03 -0330

    Updated PNG and ZLIB libraries to latest released version.

--- src/lib/libpng/pnglibconf.h
+++ src/lib/libpng/pnglibconf.h
@@ -1,8 +1,8 @@
 /* pnglibconf.h - library build configuration */
 
-/* libpng version 1.6.50 */
+/* libpng version 1.6.55 */
 
-/* Copyright (c) 2018-2025 Cosmin Truta */
+/* Copyright (c) 2018-2026 Cosmin Truta */
 /* Copyright (c) 1998-2002,2004,2006-2018 Glenn Randers-Pehrson */
 
 /* This code is released under the libpng license. */
@@ -223,7 +223,7 @@
 #define PNG_USER_HEIGHT_MAX 1000000
 #define PNG_USER_WIDTH_MAX 1000000
 #define PNG_ZBUF_SIZE 8192
-#define PNG_ZLIB_VERNUM 0x1310
+#define PNG_ZLIB_VERNUM 0x1320
 #define PNG_Z_DEFAULT_COMPRESSION (-1)
 #define PNG_Z_DEFAULT_NOFILTER_STRATEGY 0
 #define PNG_Z_DEFAULT_STRATEGY 1
