1425c36d8a153d99809ad61a451690f38188f3f4
Stephen Anthony <sa666666@gmail.com>
2026-03-27 17:43:55 -0230

    Refactor LinkedObjectPool to use vector instead of std::list. This is much more cache-friendly, and likely faster.

--- src/common/LinkedObjectPool.hxx
+++ src/common/LinkedObjectPool.hxx
@@ -18,46 +18,66 @@
 #ifndef LINKED_OBJECT_POOL_HXX
 #define LINKED_OBJECT_POOL_HXX
 
-#include <list>
 #include "bspf.hxx"
 
 /**
-  A fixed-size object-pool based doubly-linked list that makes use of
-  multiple STL lists, to reduce frequent (de)allocations.
-
-  This structure can be used as either a stack or queue, but also allows
-  for removal at any location in the list.
-
-  There are two internal lists; one stores active nodes, and the other
-  stores pool nodes that have been 'deleted' from the active list (note
-  that no actual deletion takes place; nodes are simply moved from one list
-  to another).  Similarly, when a new node is added to the active list, it
-  is simply moved from the pool list to the active list.
-
-  In all cases, the variable 'myCurrent' is updated to point to the
-  current node.
-
-  NOTE: You must always call 'currentIsValid()' before calling 'current()',
-        to make sure that the return value is a valid reference.
-
-        In the case of methods which wrap the C++ 'splice()' method, the
-        semantics of splice are followed wrt invalid/out-of-range/etc
-        iterators.  See the applicable C++ STL documentation for such
-        behaviour.
+  A fixed-size object-pool based list (based on a vector) that makes
+  no further allocations after being resized.
 
   @author Stephen Anthony
 */
 namespace Common {
 
-template <typename T, uInt32 CAPACITY = 100>
+template <typename T, uint32_t CAPACITY = 100>
 class LinkedObjectPool
 {
-  public:
-    using iter = std::list<T>::iterator;
-    using const_iter = std::list<T>::const_iterator;
+    static constexpr uInt32 npos = std::numeric_limits<uInt32>::max();
 
+    struct Node {
+      T value{};
+      uInt32 prev{npos};
+      uInt32 next{npos};
+      bool active{false};
+    };
+
+  public:
+    // =========================
+    // Iterator (STL-like)
+    // =========================
+    class const_iter {
+      public:
+        const_iter(const LinkedObjectPool* p, uInt32 i)
+          : pool(p), idx(i) {}
+
+        const T& operator*() const { return pool->myNodes[idx].value; }
+        const T* operator->() const { return &pool->myNodes[idx].value; }
+
+        const_iter& operator++() {
+          idx = (idx == npos) ? npos : pool->myNodes[idx].next;
+          return *this;
+        }
+
+        const_iter& operator--() {
+          if(idx == npos) idx = pool->myTail;
+          else idx = pool->myNodes[idx].prev;
+          return *this;
+        }
+
+        bool operator==(const const_iter& o) const { return idx == o.idx; }
+        bool operator!=(const const_iter& o) const { return idx != o.idx; }
+
+        uInt32 index() const { return idx; }
+
+      private:
+        const LinkedObjectPool* pool;
+        uInt32 idx;
+      };
+
+      using iter = const_iter;
+
+  public:
     /*
-      Create a pool of size CAPACITY; the active list starts out empty.
+      Create a pool of size CAPACITY.
     */
     LinkedObjectPool() { resize(CAPACITY); }
     ~LinkedObjectPool() = default;
@@ -65,148 +85,158 @@ class LinkedObjectPool
     /**
       Return node data that the 'current' iterator points to.
       Note that this returns a valid value only in the case where the list
-      is non-empty (at least one node has been added to the active list).
+      is non-empty (at least one node has been added).
 
       Make sure to call 'currentIsValid()' before accessing this method.
     */
-    T& current() const { return *myCurrent; }
+    T& current() {
+      assert(currentIsValid());
+      return myNodes[myCurrentIdx].value;
+    }
+    const T& current() const {
+      assert(currentIsValid());
+      return myNodes[myCurrentIdx].value;
+    }
 
     /**
-      Returns current's position in the list
+      Does the 'current' iterator point to a valid node in the list?
+      This must be called before 'current()' is called.
+    */
+    bool currentIsValid() const { return myCurrentIdx != npos; }
 
-      SLOW, but only required for messages
+    /**
+      Returns current's position in the list.
     */
     uInt32 currentIdx() const {
-      if(empty())
-        return 0;
+      if(!currentIsValid()) return 0;
 
-      iter it = myCurrent;
       uInt32 idx = 1;
+      uInt32 it = myCurrentIdx;
 
-      while(it != myList.begin()) {
+      while(myNodes[it].prev != npos) {
         ++idx;
-        --it;
+        it = myNodes[it].prev;
       }
       return idx;
     }
 
     /**
-      Does the 'current' iterator point to a valid node in the active list?
-      This must be called before 'current()' is called.
-    */
-    bool currentIsValid() const { return myCurrent != myList.end(); }
-
-    /**
-      Advance 'current' iterator to previous position in the active list.
-      If we go past the beginning, it is reset to one past the end (indicates nullptr).
+      Advance 'current' iterator to previous position in the list.
     */
     void moveToPrevious() {
       if(currentIsValid())
-        myCurrent = myCurrent == myList.begin() ? myList.end() : std::prev(myCurrent, 1);
+        myCurrentIdx = myNodes[myCurrentIdx].prev;
     }
 
     /**
-      Advance 'current' iterator to next position in the active list.
-      If we go past the last node, it will point to one past the end (indicates nullptr).
+      Advance 'current' iterator to next position in the list.
     */
     void moveToNext() {
       if(currentIsValid())
-        myCurrent = std::next(myCurrent, 1);
+        myCurrentIdx = myNodes[myCurrentIdx].next;
+      }
+
+    /**
+      Advance 'current' iterator to first position in the list.
+    */
+    void moveToFirst() { myCurrentIdx = myHead; }
+
+    /**
+      Advance 'current' iterator to last position in the list.
+    */
+    void moveToLast()  { myCurrentIdx = myTail; }
+
+    /**
+      Return an iterator to the specified node in the list.
+    */
+    const_iter first() const { return const_iter(this, myHead); }
+    const_iter last()  const { return const_iter(this, myTail); }
+    const_iter previous(const_iter i) const {
+      return const_iter(this, (i.index() == npos) ? npos : myNodes[i.index()].prev);
     }
-
-    /**
-      Advance 'current' iterator to first position in the active list.
-    */
-    void moveToFirst() {
-      if(currentIsValid())
-        myCurrent = myList.begin();
+    const_iter next(const_iter i) const {
+      return const_iter(this, (i.index() == npos) ? npos : myNodes[i.index()].next);
     }
-
-    /**
-      Advance 'current' iterator to last position in the active list.
-    */
-    void moveToLast() {
-      if(currentIsValid())
-        myCurrent = std::prev(myList.end(), 1);
-    }
-
-    /**
-      Return an iterator to the first node in the active list.
-    */
-    const_iter first() const { return myList.begin(); }
-
-    /**
-      Return an iterator to the last node in the active list.
-    */
-    const_iter last() const { return std::prev(myList.end(), 1); }
-
-    /**
-      Return an iterator to the previous node of 'i' in the active list.
-    */
-    const_iter previous(const_iter i) const { return std::prev(i, 1); }
-
-    /**
-      Return an iterator to the next node to 'current' in the active list.
-    */
-    const_iter next(const_iter i) const { return std::next(i, 1); }
 
     /**
       Canonical iterators from C++ STL.
     */
-    const_iter cbegin() const { return myList.cbegin(); }
-    const_iter cend() const   { return myList.cend();   }
+    const_iter cbegin() const { return const_iter(this, myHead); }
+    const_iter cend()   const { return const_iter(this, npos); }
 
     /**
-      Answer whether 'current' is at the specified iterator.
+      Answer whether 'current' is at the specified position.
     */
-    bool atFirst() const { return myCurrent == first(); }
-    bool atLast() const  { return myCurrent == last();  }
+    bool atFirst() const { return myCurrentIdx == myHead; }
+    bool atLast()  const { return myCurrentIdx == myTail; }
 
     /**
-      Add a new node at the beginning of the active list, and update 'current'
+      Add a new node at the beginning of the list, and update 'current'
       to point to that node.
     */
     void addFirst() {
-      myList.splice(myList.begin(), myPool, myPool.begin());
-      myCurrent = myList.begin();
+      if(full()) return;
+
+      uInt32 n = alloc();
+
+      Node& nd = myNodes[n];
+      nd.active = true;
+
+      nd.prev = npos;
+      nd.next = myHead;
+
+      if(myHead != npos) myNodes[myHead].prev = n;
+      myHead = n;
+
+      if(myTail == npos) myTail = n;
+
+      myCurrentIdx = n;
+      ++mySize;
     }
 
     /**
-      Add a new node at the end of the active list, and update 'current'
+      Add a new node at the end of the list, and update 'current'
       to point to that node.
     */
     void addLast() {
-      myList.splice(myList.end(), myPool, myPool.begin());
-      myCurrent = std::prev(myList.end(), 1);
+      if(full()) return;
+
+      const uInt32 n = alloc();
+
+      Node& nd = myNodes[n];
+      nd.active = true;
+
+      nd.prev = myTail;
+      nd.next = npos;
+
+      if(myTail != npos) myNodes[myTail].next = n;
+      myTail = n;
+
+      if(myHead == npos) myHead = n;
+
+      myCurrentIdx = n;
+      ++mySize;
     }
 
     /**
-      Remove the first node of the active list, updating 'current' if it
+      Remove the specified node of the list, updating 'current' if it
       happens to be the one removed.
     */
     void removeFirst() {
-      const_iter i = myList.cbegin();
-      myPool.splice(myPool.end(), myList, i);
-      if(myCurrent == i)  // did we just invalidate 'current'
-        moveToNext();     // if so, move to the next node
+      if(myHead != npos)
+        removeNode(myHead);
     }
-
-    /**
-      Remove the last node of the active list, updating 'current' if it
-      happens to be the one removed.
-    */
     void removeLast() {
-      const_iter i = std::prev(myList.end(), 1);
-      myPool.splice(myPool.end(), myList, i);
-      if(myCurrent == i)  // did we just invalidate 'current'
-        moveToPrevious(); // if so, move to the previous node
+      if(myTail != npos)
+        removeNode(myTail);
     }
 
     /**
-      Remove a single element from the active list at position of the iterator.
+      Remove a single element from the list at position of the iterator.
     */
     void remove(const_iter i) {
-      myPool.splice(myPool.end(), myList, i);
+      if(i.index() != npos)
+        removeNode(i.index());
     }
 
     /**
@@ -215,24 +245,30 @@ class LinkedObjectPool
       and so on).
     */
     void remove(uInt32 index) {
-      myPool.splice(myPool.end(), myList, std::next(myList.begin(), index));
+      if(index >= mySize) return;
+
+      uInt32 it = myHead;
+      while(index--)
+        it = myNodes[it].next;
+
+      removeNode(it);
     }
 
     /**
-      Remove range of elements from the beginning of the active list to
+      Remove range of elements from the beginning of the list to
       the 'current' node.
     */
     void removeToFirst() {
-      myPool.splice(myPool.end(), myList, myList.begin(), myCurrent);
+      while(myHead != npos && myHead != myCurrentIdx)
+        removeNode(myHead);
     }
 
     /**
-      Remove range of elements from the node after 'current' to the end of the
-      active list.
+      Remove range of elements from the node after 'current' to the end of the list.
     */
     void removeToLast() {
-      if(currentIsValid())
-        myPool.splice(myPool.end(), myList, std::next(myCurrent, 1), myList.end());
+      while(myTail != npos && myTail != myCurrentIdx)
+        removeNode(myTail);
     }
 
     /**
@@ -240,44 +276,85 @@ class LinkedObjectPool
       (ie, the list essentially becomes empty again).
     */
     void resize(uInt32 capacity) {
-      if(myCapacity != capacity)  // only resize when necessary
-      {
-        myList.clear();  myPool.clear();
-        myCurrent = myList.end();
-        myCapacity = capacity;
+      if(capacity == myCapacity) return;
 
-        for(uInt32 i = 0; i < myCapacity; ++i)
-          myPool.emplace_back(T());
-      }
+      myCapacity = capacity;
+      myNodes.resize(myCapacity);
+      clear();
     }
 
     /**
       Erase entire contents of active list.
     */
     void clear() {
-      myPool.splice(myPool.end(), myList, myList.begin(), myList.end());
-      myCurrent = myList.end();
+      for(uInt32 i = 0; i < myCapacity; ++i) {
+        myNodes[i].next = i + 1;
+        myNodes[i].prev = npos;
+        myNodes[i].active = false;
+      }
+
+      myNodes[myCapacity - 1].next = npos;
+
+      myFreeHead = 0;
+      myHead = myTail = myCurrentIdx = npos;
+      mySize = 0;
     }
 
     uInt32 capacity() const { return myCapacity; }
+    uInt32 size() const { return mySize; }
+    bool empty() const { return mySize == 0; }
+    bool full()  const { return mySize == myCapacity; }
 
-    uInt32 size() const { return static_cast<uInt32>(myList.size()); }
-    bool empty() const  { return size() == 0; }
-    bool full() const   { return size() >= capacity(); }
-
+  #if 0
     friend ostream& operator<<(ostream& os, const LinkedObjectPool<T>& p) {
       for(const auto& i: p.myList)
         os << i << (p.current() == i ? "* " : "  ");
       return os;
     }
+  #endif
 
   private:
-    std::list<T> myList, myPool;
+    uInt32 alloc() {
+      const uInt32 n = myFreeHead;
+      myFreeHead = myNodes[n].next;
+      return n;
+    }
 
-    // Current position in the active list (end() indicates an invalid position)
-    iter myCurrent{myList.end()};
+    void freeNode(uInt32 n) {
+      myNodes[n].next = myFreeHead;
+      myNodes[n].active = false;
+      myFreeHead = n;
+    }
 
-    // Total capacity of the pool
+    void removeNode(uInt32 n) {
+      const Node& nd = myNodes[n];
+
+      const uInt32 p = nd.prev;
+      const uInt32 nx = nd.next;
+
+      if(p != npos) myNodes[p].next = nx;
+      else myHead = nx;
+
+      if(nx != npos) myNodes[nx].prev = p;
+      else myTail = p;
+
+      if(myCurrentIdx == n)
+        myCurrentIdx = nx;
+
+      freeNode(n);
+      --mySize;
+    }
+
+  private:
+    std::vector<Node> myNodes;
+
+    uInt32 myHead{npos};
+    uInt32 myTail{npos};
+    uInt32 myCurrentIdx{npos};
+
+    uInt32 myFreeHead{npos};
+
+    uInt32 mySize{0};
     uInt32 myCapacity{0};
 
   private:
