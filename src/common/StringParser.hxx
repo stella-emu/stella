1d9d10e0f2b7b8ccfc43e94b1639b064a33e7950
Stephen Anthony <sa666666@gmail.com>
2026-03-28 14:54:19 -0230

    Optimize StringParser for string_view.  Still need changes to StringList to take full advantage of this.

--- src/common/StringParser.hxx
+++ src/common/StringParser.hxx
@@ -24,6 +24,11 @@
   This class converts a string into a StringList by splitting on a delimiter
   and size.
 
+  TODO: This class is actually optimized to work with and store string_view.
+        However, StringList currently stores string, not string_view.
+        This is still a little more efficient, but would be *much* more
+        efficient if we could convert StringList somehow.
+
   @author Stephen Anthony
 */
 class StringParser
@@ -35,14 +40,10 @@ class StringParser
       @param str  The string to split
     */
     explicit StringParser(string_view str)
+      : myBuffer{str}
     {
-      istringstream buf(string{str});  // TODO: fixed in C++23
-      string line;
-
-      while(std::getline(buf, line, '\n'))
-        myStringList.push_back(line);
+      parseLines();
     }
-    ~StringParser() = default;
 
     /**
       Split the given string based on the newline character, making sure that
@@ -52,39 +53,76 @@ class StringParser
       @param maxlen The maximum length of string to generate
     */
     StringParser(string_view str, uInt32 maxlen)
+      : myBuffer(str)
     {
-      istringstream buf(string{str});  // TODO: fixed in C++23
-      string line;
-
-      while(std::getline(buf, line, '\n'))
-      {
-        size_t len = maxlen;
-        const size_t size = line.size();
-
-        if(size <= len)
-          myStringList.push_back(line);
-        else
-        {
-          size_t beg = 0;
-          while((beg + maxlen) < size)
-          {
-            const size_t spos = line.find_last_of(' ', beg + len);
-            if(spos != string::npos && spos > beg)
-              len = spos - beg;
-
-            myStringList.push_back(line.substr(beg, len));
-            beg += len + 1;
-            len = maxlen;
-          }
-          myStringList.push_back(line.substr(beg));
-        }
-      }
+      parseWrapped(maxlen);
     }
+
+    ~StringParser() = default;
 
     const StringList& stringList() const { return myStringList; }
 
   private:
+    string myBuffer;
     StringList myStringList;
+
+    void parseLines()
+    {
+      size_t start = 0;
+      const size_t n = myBuffer.size();
+
+      while(start < n)
+      {
+        size_t end = myBuffer.find('\n', start);
+        if(end == string::npos)
+          end = n;
+
+        myStringList.emplace_back(myBuffer.data() + start, end - start);
+        start = end + 1;
+      }
+    }
+
+    void parseWrapped(size_t maxlen)
+    {
+      size_t start = 0;
+      const size_t n = myBuffer.size();
+
+      while(start < n)
+      {
+        size_t end = myBuffer.find('\n', start);
+        if(end == string::npos)
+          end = n;
+
+        wrapLine(string_view(myBuffer.data() + start, end - start), maxlen);
+        start = end + 1;
+      }
+    }
+
+    void wrapLine(string_view line, size_t maxlen)
+    {
+      size_t beg = 0;
+
+      while(beg < line.size())
+      {
+        const size_t len = std::min(maxlen, line.size() - beg);
+        size_t split = beg + len;
+
+        if(split < line.size())
+        {
+          const size_t space = line.rfind(' ', split);
+          if(space != string_view::npos && space > beg)
+            split = space;
+        }
+
+        myStringList.emplace_back(line.data() + beg, split - beg);
+
+        // Skip space if we split on one
+        if(split < line.size() && line[split] == ' ')
+          beg = split + 1;
+        else
+          beg = split;
+      }
+    }
 
   private:
     // Following constructors and assignment operators not supported
